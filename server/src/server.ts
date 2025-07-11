7da02076564f601467052ebc8b7d475072772eec
RuMax21 <cherednikov.maxim@mail.ru>
2025-07-11 14:07:30 +0300

    project setup with express & basic server

---
+++ server/src/server.ts
@@ -0,0 +1,11 @@
+import { config } from "dotenv";
+import http from "http";
+import app from "./app";
+
+const PORT = process.env.SERVER_PORT || 3000;
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+})
